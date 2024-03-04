1cb3916830af3a91cd92a91042041158df6b6850
HMaroba <marobahlalele910@gmail.com>
2024-03-04 10:35:27 +0200

    Initial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,12 @@ import React from "react";
 export default () => {
   return (
     <Tabs>
-      <Tabs.Screen name="home " />
+      <Tabs.Screen
+        name="home "
+        options={{
+          headerTitle: "Home Page",
+        }}
+      />
       <Tabs.Screen name="list " />
     </Tabs>
   );
