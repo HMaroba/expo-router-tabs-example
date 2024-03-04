60fb647c5d5710123496a201b08af7a555eccc72
HMaroba <marobahlalele910@gmail.com>
2024-03-04 10:21:49 +0200

    Created stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const StackLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 };
