cdb343f34eb01b3eaba9819b1ac7d42c8a61d737
HMaroba <marobahlalele910@gmail.com>
2024-03-04 10:44:20 +0200

    Initial

--- app/(tabs)/list/[id].tsx
+++ app/(tabs)/list/[id].tsx
@@ -1,15 +1,20 @@
-import { Stack } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { View, Text } from "react-native";
 
 const DetailsPage = () => {
+  const { id } = useLocalSearchParams();
   return (
     <View>
       <Stack.Screen
         options={{
-          headerTitle: "Details of ",
+          headerTitle: `Details of ${id}`,
         }}
       />
+
+      <Text>Details for {id}</Text>
     </View>
   );
 };
+
+export default DetailsPage;
